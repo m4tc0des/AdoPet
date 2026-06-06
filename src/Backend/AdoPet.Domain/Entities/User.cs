6b6bddc73aa518a445ff842fc99ab5adf9a9376f
m4tc0des <matt_ufo2@hotmail.com>
2026-06-06 20:34:58 -0300

    feat: adicionada entidade de usuario

---
+++ src/Backend/AdoPet.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace AdoPet.Domain.Entities;
+
+public class User
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
