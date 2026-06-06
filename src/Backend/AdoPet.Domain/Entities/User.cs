99bf95e42e7bc42520935f4b8b345d721a090682
m4tc0des <matt_ufo2@hotmail.com>
2026-06-06 20:36:36 -0300

    f

--- src/Backend/AdoPet.Domain/Entities/User.cs
+++ src/Backend/AdoPet.Domain/Entities/User.cs
@@ -2,6 +2,9 @@
 
 public class User
 {
+    public long Id { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public string UserName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
