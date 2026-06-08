da7118c01bac7350167ae69d65f5172c73a89df0
m4tc0des <matt_ufo2@hotmail.com>
2026-06-08 20:55:43 -0300

    feat: criada interface e estrutura de pastas pra hashear a senha

---
+++ src/Backend/AdoPet.Domain/Security/PasswordHashing/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace AdoPet.Domain.Security.PasswordHashing;
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string passwordHash);
+}
---
+++ src/Backend/AdoPet.Infrastructure/Security/PasswordHashing/Argon2PasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoPet.Infrastructure.Security.PasswordHashing;
+
+internal class Argon2PasswordHasher
+{
+}
