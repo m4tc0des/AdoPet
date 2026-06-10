e3ae776dc38fa7898b322f6ba989891f36e227fe
m4tc0des <matt_ufo2@hotmail.com>
2026-06-10 19:08:16 -0300

    refact: adicionado função de validar request para deixar o execute mais limpa

--- src/Backend/AdoPet.Application/UseCases/User/Register/RegisterUserAccountJseCase.cs
+++ src/Backend/AdoPet.Application/UseCases/User/Register/RegisterUserAccountJseCase.cs
@@ -1,11 +1,19 @@
 ﻿using AdoPet.Communication.Requests;
 using AdoPet.Exception.ExceptionsBase;
+using Mapster;
 
 namespace AdoPet.Application.UseCases.User.Register;
 
 public class RegisterUserAccountJseCase
 {
     public void Excute(RequestsRegisterUserJson request)
+    {
+        ValidateAndThrowOnValidation(request);
+
+        var user = request.Adapt<Domain.Entities.User>();
+    }
+
+    public void ValidateAndThrowOnValidation(RequestsRegisterUserJson request)
     {
         var validator = new RegisterUserAccountValidator();
 
