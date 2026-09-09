6267a7d51a73ccf4b546c71eadf60d520d99d54e
m4tc0des <matt_ufo2@hotmail.com>
2026-09-08 22:29:21 -0300

    feat(infra): registra DbContext, UnitOfWork e repositorios na injeção de dependência

--- src/Backend/AdoPet.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/AdoPet.Infrastructure/DependencyInjectionExtension.cs
@@ -1,6 +1,10 @@
-﻿using AdoPet.Domain.Repositories.User;
+﻿using AdoPet.Domain.Repositories;
+using AdoPet.Domain.Repositories.User;
 using AdoPet.Domain.Security.PasswordHashing;
+using AdoPet.Infrastructure.DataAccess;
 using AdoPet.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Infrastructure.Security.PasswordHashing;
 
@@ -10,11 +14,23 @@ public static class DependencyInjectionExtension
 {
     extension(IServiceCollection services)
     {
-        public void AddInfrastructure()
+        public void AddInfrastructure(IConfiguration configuration)
         {
             services.AddScoped<IPasswordHasher, Argon2PasswordHasher>();
 
             services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AdoPetDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DbConnection");
+
+                options.UseMySQL(connectionString!);
+            });
+                
         }
     }
 }
