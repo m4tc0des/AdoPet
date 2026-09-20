2683a32b84d12e764fec8b7580cc05f4606e4d19
m4tc0des <matt_ufo2@hotmail.com>
2026-09-20 10:22:33 -0300

    refactor(exception): adiciona metodos abstratos GetErrorsMessages e GetStatusCode em AdoPetException

--- src/Shared/AdoPet.Exception/ExceptionsBase/AdoPetException.cs
+++ src/Shared/AdoPet.Exception/ExceptionsBase/AdoPetException.cs
@@ -1,5 +1,9 @@
-﻿namespace AdoPet.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace AdoPet.Exception.ExceptionsBase;
 
 public abstract class AdoPetException: System.Exception
 {
+    public abstract List<string> GetErrorsMessages();
+    public abstract HttpStatusCode GetStatusCode();
 }
--- src/Shared/AdoPet.Exception/ExceptionsBase/ErrorOnValidationException.cs
+++ src/Shared/AdoPet.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,15 +1,23 @@
-﻿namespace AdoPet.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace AdoPet.Exception.ExceptionsBase;
 
 public class ErrorOnValidationException : AdoPetException
 {
     private readonly List<string> _errors;
+
     public ErrorOnValidationException(List<string> errorMessages)
     {
         _errors = errorMessages;
     }
 
-    public List<string> GetErrorsMessages()
+    public override List<string> GetErrorsMessages()
     {
         return _errors;
+    }
+
+    public override HttpStatusCode GetStatusCode()
+    {
+        return HttpStatusCode.BadRequest;
     }
 }
