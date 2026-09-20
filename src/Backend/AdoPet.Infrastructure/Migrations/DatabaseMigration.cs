a2d4d1cc506be30451a3fdcc524d210f0e766992
m4tc0des <matt_ufo2@hotmail.com>
2026-09-20 10:18:07 -0300

    feat(infra): cria classe DatabaseMigration para execucao automatica de migracao no banco

---
+++ src/Backend/AdoPet.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,15 @@
+﻿using AdoPet.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdoPet.Infrastructure.Migrations;
+
+public static class DatabaseMigration
+{
+    public static async Task MigrateDatabaseAsync(IServiceProvider serviceProvider)
+    {
+        var dbContext = serviceProvider.GetRequiredService<AdoPetDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
