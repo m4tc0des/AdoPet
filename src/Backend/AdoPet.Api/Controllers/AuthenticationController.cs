9137977e9b00080aa56ce7c71ee01bb0b44546e5
m4tc0des <matt_ufo2@hotmail.com>
2026-09-21 22:30:56 -0300

    feat(api/communication): cria DTO RequestLoginJson e AuthenticationController com endpoint de login

---
+++ src/Backend/AdoPet.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using AdoPet.Application.UseCases.Login.LoginWithEmailAndPassword;
+using AdoPet.Communication.Requests;
+using AdoPet.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdoPet.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> LoginWithEmail([FromBody] RequestLoginJson request, [FromServices] ILoginWithEmailAndPasswordUseCase useCase)
+    {
+        var response = await useCase.Execute(request);
+
+        return Ok(response);
+    }
+}
