3017ce5137c15637ed54ddce5a7cf097a6bf85a5
m4tc0des <matt_ufo2@hotmail.com>
2026-09-21 22:31:52 -0300

    feat(api): configura JsonOptions e executa migracoes do banco na inicializacao

--- src/Backend/AdoPet.Api/Program.cs
+++ src/Backend/AdoPet.Api/Program.cs
@@ -1,10 +1,14 @@
 using AdoPet.Infrastructure;
 using AdoPet.Api.Filters;
 using AdoPet.Application;
+using AdoPet.Api.Converters;
+using AdoPet.Infrastructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddOpenApi();
 
@@ -18,7 +22,7 @@ builder.Services.AddMvc(options => options.Filters.Add<ExceptionFilter>());
 
 builder.Services.AddApplication();
 
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -37,4 +41,15 @@ app.UseAuthorization();
  
 app.MapControllers();
 
+await ExecuteMigrations();
+
 app.Run();
+
+async Task ExecuteMigrations()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+
+    await DatabaseMigration.MigrateDatabaseAsync(scope.ServiceProvider);
+}
+
+public partial class Program { }
