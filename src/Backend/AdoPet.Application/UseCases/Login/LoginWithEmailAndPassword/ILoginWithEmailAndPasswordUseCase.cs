1f80621f253a591e0851a9ff37464818267fbd31
m4tc0des <matt_ufo2@hotmail.com>
2026-09-21 22:32:57 -0300

    feat(usecase): adiciona validacoes de credenciais com InvalidLoginException no LoginWithEmailAndPasswordUseCase

---
+++ src/Backend/AdoPet.Application/UseCases/Login/LoginWithEmailAndPassword/ILoginWithEmailAndPasswordUseCase.cs
@@ -0,0 +1,9 @@
+﻿using AdoPet.Communication.Requests;
+using AdoPet.Communication.Responses;
+
+namespace AdoPet.Application.UseCases.Login.LoginWithEmailAndPassword;
+
+public interface ILoginWithEmailAndPasswordUseCase
+{
+    Task<ResponseRegisterUserJson> Execute(RequestLoginJson request);
+}
---
+++ src/Backend/AdoPet.Application/UseCases/Login/LoginWithEmailAndPassword/LoginWithEmailAndPasswordUseCase.cs
@@ -0,0 +1,35 @@
+﻿using AdoPet.Communication.Requests;
+using AdoPet.Communication.Responses;
+using AdoPet.Domain.Repositories.User;
+using AdoPet.Domain.Security.PasswordHashing;
+using AdoPet.Exception.ExceptionsBase;
+
+namespace AdoPet.Application.UseCases.Login.LoginWithEmailAndPassword;
+
+public class LoginWithEmailAndPasswordUseCase : ILoginWithEmailAndPasswordUseCase
+{
+    private readonly IUserReadOnlyRepository _userReadRepository;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public LoginWithEmailAndPasswordUseCase(IUserReadOnlyRepository userReadRepository, IPasswordHasher passwordHasher)
+    {
+        _userReadRepository = userReadRepository;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<ResponseRegisterUserJson> Execute(RequestLoginJson request)
+    {
+        var user = await _userReadRepository.GetByEmail(request.Email);
+
+        if (user is null) throw new InvalidLoginException();
+
+        var isPasswordValid = _passwordHasher.VerifyPassword(request.Password, user.Password);
+
+        if (isPasswordValid == false) throw new InvalidLoginException();
+
+        return new ResponseRegisterUserJson
+        {
+            UserName = user.UserName
+        };
+    }
+}
