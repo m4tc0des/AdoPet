8ce145ab2109da9ffb41e18335312e79f89fbc09
m4tc0des <matt_ufo2@hotmail.com>
2026-09-23 16:46:03 -0300

    feat(security): adiciona interface IAccessTokenGenerator e implementa JwtTokenHandler

---
+++ src/Backend/AdoPet.Domain/Security/Tokens/IAccessTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using AdoPet.Domain.Entities;
+
+namespace AdoPet.Domain.Security.Tokens;
+
+public interface IAccessTokenGenerator
+{
+    string Generate(User user);
+}
---
+++ src/Backend/AdoPet.Infrastructure/Security/Tokens/AccessToken/JwtTokenHandler.cs
@@ -0,0 +1,45 @@
+﻿using AdoPet.Domain.Entities;
+using AdoPet.Domain.Security.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace AdoPet.Infrastructure.Security.Tokens.AccessToken;
+
+internal sealed class JwtTokenHandler : IAccessTokenGenerator
+{
+    private readonly uint _expireTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenHandler(uint expireTimeMinutes, string signingKey)
+    {
+        _expireTimeMinutes = expireTimeMinutes;
+        _signingKey = signingKey;
+    }
+
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new (JwtRegisteredClaimNames.Sub, user.Id.ToString())
+        };
+        var tokenDescription = new SecurityTokenDescriptor()
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expireTimeMinutes),
+            SigningCredentials = new SigningCredentials(Credentials(), SecurityAlgorithms.HmacSha256),
+            Subject = new ClaimsIdentity(claims)
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        return handler.CreateToken(tokenDescription);
+    }
+
+    private SymmetricSecurityKey Credentials()
+    {
+        var signingKey = Encoding.UTF8.GetBytes(_signingKey);
+
+        return new SymmetricSecurityKey(signingKey);
+    }    
+}
