d1176bd8516144073d1b0138a09b0d97a705fa03
m4tc0des <matt_ufo2@hotmail.com>
2026-09-25 23:35:14 -0300

    test(application): adiciona testes unitários para o caso de uso de registro de usuário
    - Implementa o teste de sucesso (Success) validando a criação correta do usuário e o retorno do token de acesso.
    - Adiciona testes baseados em Teoria (Theory e ClassData) para validar o lançamento de exceções quando o nome de usuário for inválido ou vazio.
    - Adiciona teste para validar exceção de e-mail já cadastrado (EmailAlreadyExists).
    - Utiliza Shouldly para asserções limpas e estrutura mocks isolados para repositórios e serviços de segurança.

---
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -0,0 +1,105 @@
+﻿using AdoPet.Application.UseCases.User.Register;
+using AdoPet.Exception;
+using CommonTestUtilities.ClassDataGenerator;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace Validators.Tests.User.Register;
+
+public class RegisterUserAccountValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var request = RequestsRegisterUserJsonBuilder.Build();
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Theory]
+    [ClassData(typeof(ErrorsGenerator))]
+    public void Validate_ShouldHaveError_When_UserNameIsEmpty(string userName)
+    {
+        var request = RequestsRegisterUserJsonBuilder.Build();
+
+        request.UserName = userName;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_USERNAME_REQUIRED));
+        });
+    }
+
+    [Theory]
+    [ClassData(typeof(ErrorsGenerator))]
+    public void Validate_ShouldHaveError_When_EmailIsEmpty(string email)
+    {
+        var request = RequestsRegisterUserJsonBuilder.Build();
+
+        request.Email = email;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_When_EmailIsInvalid()
+    {
+        var request = RequestsRegisterUserJsonBuilder.Build();
+
+        request.Email = "invalidemail.com";
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
+    }
+
+    [Theory]
+    [ClassData(typeof(ErrorsGenerator))]
+    public void Validate_ShouldHaveError_When_PasswordIsEmpty(string password)
+    {
+        var request = RequestsRegisterUserJsonBuilder.Build();
+
+        request.Password = password;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+}
